64448917ef188b60879fe8aeaccac76ff79b96b3
jhpy1024 <jhpy1024@gmail.com>
2014-09-02 16:05:34 +0100

    Started Exercise 1-23

---
+++ exercise_1-23.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define IN_STRING_LITERAL 0
+#define IN_CHARACTER_LITERAL 1
+
+int main()
+{
+    int current_char = EOF;
+    bool should_print = true;
+
+    while ((current_char = getchar()) != EOF)
+    {
+
+    }
+
+    return 0;
+}
