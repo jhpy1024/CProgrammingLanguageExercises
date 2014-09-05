6e474436fda3c18b693e651df22060ac479569de
jhpy1024 <jhpy1024@gmail.com>
2014-09-05 20:25:32 +0100

    Exercise 1-24 now supports checking for too many closing parens/brackets/braces

--- exercise_1-24.c
+++ exercise_1-24.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #define OPEN_PARENTHESIS    '('
 #define CLOSE_PARENTHESIS   ')'
@@ -17,6 +18,19 @@
 
 #define MAX_INPUT_LENGTH 100000
 
+int literal_state;
+int comment_state;
+int current_char;
+int next_char;
+
+void init_input_buffer(int input[])
+{
+    for (int i = 0; i < MAX_INPUT_LENGTH; ++i)
+    {
+        input[i] = '\0';
+    }
+}
+
 void read_input(int input[])
 {
     int current_char = EOF;
@@ -30,16 +44,72 @@ void read_input(int input[])
 
 void print_error(int line, char message[])
 {
-    printf("[line %d] %s\n", line, message);
+    printf("[error @ line %d] %s\n", line, message);
+}
+
+void print_error_if(int line, char message[], bool condition)
+{
+    if (condition)
+    {
+        print_error(line, message);
+    }
+}
+
+bool in_code()
+{
+    return literal_state == NOT_IN_LITERAL && comment_state == NOT_IN_COMMENT;
+}
+
+bool in_comment()
+{
+    return comment_state == NOT_IN_COMMENT;
+}
+
+bool in_single_line_comment()
+{
+    return comment_state == IN_SINGLE_LINE_COMMENT;
+}
+
+bool in_multi_line_comment()
+{
+    return comment_state == IN_MULTI_LINE_COMMENT;
+}
+
+bool in_string()
+{
+    return comment_state == IN_STRING;
+}
+
+bool in_character()
+{
+    return comment_state == IN_CHARACTER;
+}
+
+bool start_of_single_line_comment()
+{
+    return current_char == '/' && next_char == '/';
+}
+
+bool start_of_multi_line_comment()
+{
+    return current_char == '/' && next_char == '*';
+}
+
+bool end_of_multi_line_comment()
+{
+    return current_char == '*' && next_char == '/';
 }
 
 int main()
 {
     int input[MAX_INPUT_LENGTH];
+    init_input_buffer(input);
     read_input(input);
 
-    int literal_state = NOT_IN_LITERAL;
-    int comment_state = NOT_IN_COMMENT;
+    literal_state = NOT_IN_LITERAL;
+    comment_state = NOT_IN_COMMENT;
+    current_char = '\0';
+    next_char = '\0';
 
     int line_number = 1;
 
@@ -49,6 +119,77 @@ int main()
 
     for (int i = 0; input[i] != '\0' && i < MAX_INPUT_LENGTH; ++i)
     {
+        current_char = input[i];
+        next_char = ((i == MAX_INPUT_LENGTH - 1) ? '\0' : input[i + 1]);
+
+        if (in_code())
+        {
+            if (start_of_single_line_comment())
+            {
+                comment_state = IN_SINGLE_LINE_COMMENT;
+            }
+            else if (start_of_multi_line_comment())
+            {
+                comment_state = IN_MULTI_LINE_COMMENT;
+            }
+            else if (current_char == OPEN_PARENTHESIS)
+            {
+                ++num_parens;
+            }
+            else if (current_char == CLOSE_PARENTHESIS)
+            {
+                --num_parens;
+                print_error_if(line_number, "too many closing parentheses", num_parens < 0);
+            }
+            else if (current_char == OPEN_BRACE)
+            {
+                ++num_braces;
+            }
+            else if (current_char == CLOSE_BRACE)
+            {
+                --num_braces;
+                print_error_if(line_number, "too many closing braces", num_braces < 0);
+            }
+            else if (current_char == OPEN_BRACKET)
+            {
+                ++num_brackets;
+            }
+            else if (current_char == CLOSE_BRACKET)
+            {
+                --num_brackets;
+                print_error_if(line_number, "too many closing brackets", num_brackets < 0);
+            }
+        }
+        else if (in_single_line_comment())
+        {
+            if (current_char == '\n')
+            {
+                comment_state = NOT_IN_COMMENT;
+            }
+        }
+        else if (in_multi_line_comment())
+        {
+            if (end_of_multi_line_comment())
+            {
+                comment_state = NOT_IN_COMMENT;
+            }
+        }
+        else if (in_string())
+        {
+            /* TODO: Support escaped double-quote ("\"") */
+            if (current_char == '"')
+            {
+                literal_state = NOT_IN_LITERAL;
+            }
+        }
+        else if (in_character())
+        {
+            /* TODO: Support escaped single-quote ('\'') */
+            if (current_char == '\'')
+            {
+                literal_state = NOT_IN_LITERAL;
+            }
+        }
     }
 
     return 0;
