680d6839bb081c4d916821f1defe4baed878b6d1
jhpy1024 <jhpy1024@gmail.com>
2014-09-11 20:56:03 +0100

    Started Exercise 2-7

---
+++ exercise_2-7.c
@@ -0,0 +1,8 @@
+/* 
+ * Write a function invert(x, p, n) that returns x with the n bits that begin at position p
+ * inverted, leaving the others unchanged.
+ */
+unsigned invert(unsigned x, int p, int n)
+{
+    unsigned bitmask = ~(~0 << n);
+}
