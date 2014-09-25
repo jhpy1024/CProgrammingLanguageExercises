569145e08fb54d2e782a1b04a59836449e442859
jhpy1024 <jhpy1024@gmail.com>
2014-09-25 19:07:23 +0100

    Exercise 5-3

---
+++ exercise_5-3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+/* Assumes that s is large enough. */
+/* Use of prefix to avoid conflics with strcat declared in string.h. */
+void my_strcat(char* s, char* t)
+{
+    s = s + strlen(s);
+
+    while ((*s++ = *t++) != '\0')
+    {
+        ;
+    }
+}
+
+int main()
+{
+    char s[100] = "This ";
+    char t[100] = "is a test";
+
+    printf("\"%s\" + \"%s\"\n", s, t);
+    strcat(s, t);
+    printf("= \"%s\"\n", s);
+
+    return 0;
+}
